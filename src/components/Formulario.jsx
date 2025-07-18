7f0c8209bae9b7d03a7e754365fdac9b66a627f6
Alanolivo <olivopalanxaviero@gmail.com>
2025-07-17 23:01:29 -0500

    Formulario.jsx

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -66,7 +66,7 @@ export function Formulario({ setUser }) {
           />
           {error && <div className="text-danger text-center">{error}</div>}
           <ReCAPTCHA
-            sitekey="6LcIzIUrAAAAAPKML-EF4BXYPkL-2YxaJSrCVIXH"
+            sitekey="6LcIzIUrAAAAAMcdYy8pWVXuRwlkfWzPoSKND8fl"
             onChange={(value) => setCapVal(value)}
           />
           <button disabled={!capVal} className="btn btn-primary" type="submit">
